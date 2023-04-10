c24c7d0b5253ff0e6e7aed68e32aaf226f348f96
Mike Kucera <mikekucera@gmail.com>
2023-04-10 09:19:05 -0400

    move action to edge table toobar, bump version to 0.2.1

--- src/main/java/org/cytoscape/mapSourceAndTarget/internal/CyActivator.java
+++ src/main/java/org/cytoscape/mapSourceAndTarget/internal/CyActivator.java
@@ -1,63 +1,53 @@
 package org.cytoscape.mapSourceAndTarget.internal;
 
-import static org.cytoscape.work.ServiceProperties.COMMAND;
-import static org.cytoscape.work.ServiceProperties.COMMAND_DESCRIPTION;
-import static org.cytoscape.work.ServiceProperties.COMMAND_EXAMPLE_JSON;
-import static org.cytoscape.work.ServiceProperties.COMMAND_LONG_DESCRIPTION;
-import static org.cytoscape.work.ServiceProperties.COMMAND_NAMESPACE;
-import static org.cytoscape.work.ServiceProperties.COMMAND_SUPPORTS_JSON;
-import static org.cytoscape.work.ServiceProperties.ID;
-import static org.cytoscape.work.ServiceProperties.IN_MENU_BAR;
-import static org.cytoscape.work.ServiceProperties.IN_TOOL_BAR;
-import static org.cytoscape.work.ServiceProperties.INSERT_SEPARATOR_BEFORE;
-import static org.cytoscape.work.ServiceProperties.LARGE_ICON_URL;
-import static org.cytoscape.work.ServiceProperties.MENU_GRAVITY;
-import static org.cytoscape.work.ServiceProperties.PREFERRED_MENU;
-import static org.cytoscape.work.ServiceProperties.TITLE;
-import static org.cytoscape.work.ServiceProperties.TOOL_BAR_GRAVITY;
-import static org.cytoscape.work.ServiceProperties.TOOLTIP;
-
-import org.cytoscape.application.CyApplicationManager;
-import org.cytoscape.model.events.NetworkAddedListener;
-import org.cytoscape.service.util.AbstractCyActivator;
-import org.cytoscape.service.util.CyServiceRegistrar;
-import org.cytoscape.work.TaskFactory;
-
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.cytoscape.work.ServiceProperties.*;
 
 import java.util.Properties;
 
 import org.cytoscape.mapSourceAndTarget.internal.model.MapSandTManager;
 import org.cytoscape.mapSourceAndTarget.internal.tasks.MapTaskFactory;
+import org.cytoscape.model.events.NetworkAddedListener;
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.task.TableTaskFactory;
+import org.cytoscape.util.swing.IconManager;
+import org.cytoscape.util.swing.TextIcon;
+import org.osgi.framework.BundleContext;
 
 
 public class CyActivator extends AbstractCyActivator {
 
-	public CyActivator() {
-		super();
-	}
-
 	public void start(BundleContext bc) {
-		final CyServiceRegistrar serviceRegistrar = getService(bc, CyServiceRegistrar.class);
-    MapSandTManager manager = new MapSandTManager(serviceRegistrar);
-    registerService(bc, manager, NetworkAddedListener.class, new Properties());
+		final CyServiceRegistrar registrar = getService(bc, CyServiceRegistrar.class);
+		MapSandTManager manager = new MapSandTManager(registrar);
+		registerService(bc, manager, NetworkAddedListener.class, new Properties());
 
 		{
-			MapTaskFactory expExp = new MapTaskFactory(serviceRegistrar);
+			var iconManager = registrar.getService(IconManager.class);
+			var iconFont = iconManager.getIconFont("cytoscape-3", 18.0f);
+			var icon = new TextIcon("#", iconFont, 24, 24); // "#" is the node table icon in the cytoscape-3 font
+			var iconId = "mapSourceAndTarget_tableTaskFactory";
+			iconManager.addIcon(iconId, icon);
+			
+			MapTaskFactory expExp = new MapTaskFactory(registrar);
 			Properties props = new Properties();
-			props.setProperty(IN_MENU_BAR, "true");
-			props.setProperty(PREFERRED_MENU, "Apps");
-			props.setProperty(TITLE, "mapSourceAndTarget");
+			
+			props.setProperty(LARGE_ICON_ID, iconId);
+			props.setProperty(TOOLTIP, "Map node source/target columns...");
+			props.setProperty("inNodeTableToolBar", "false");
+			props.setProperty("inEdgeTableToolBar", "true");
+			props.setProperty("inNetworkTableToolBar", "false");
+			props.setProperty("inUnassignedTableToolBar", "false");
+			props.setProperty(TOOL_BAR_GRAVITY, "0.0045");
+			
 			props.setProperty(COMMAND_NAMESPACE, "mapSourceAndTarget");
 			props.setProperty(COMMAND, "map");
 			props.setProperty(COMMAND_DESCRIPTION, "Map node columns into the edge table");
 			props.setProperty(COMMAND_LONG_DESCRIPTION, "");
 			props.setProperty(COMMAND_SUPPORTS_JSON, "true");
 			props.setProperty(COMMAND_EXAMPLE_JSON, "{}");
-			registerService(bc, expExp, TaskFactory.class, props);
+			
+			registerService(bc, expExp, TableTaskFactory.class, props);
 		}
 
 	}
--- src/main/java/org/cytoscape/mapSourceAndTarget/internal/tasks/MapTaskFactory.java
+++ src/main/java/org/cytoscape/mapSourceAndTarget/internal/tasks/MapTaskFactory.java
@@ -1,15 +1,17 @@
 package org.cytoscape.mapSourceAndTarget.internal.tasks;
 
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNetworkTableManager;
+import org.cytoscape.model.CyTable;
 import org.cytoscape.service.util.CyServiceRegistrar;
+import org.cytoscape.task.TableTaskFactory;
 import org.cytoscape.work.AbstractTaskFactory;
-import org.cytoscape.work.TaskFactory;
 import org.cytoscape.work.TaskIterator;
 
-public class MapTaskFactory extends AbstractTaskFactory {
+public class MapTaskFactory extends AbstractTaskFactory implements TableTaskFactory {
   final CyServiceRegistrar registrar;
 
   public MapTaskFactory(final CyServiceRegistrar reg) {
-    super();
     this.registrar = reg;
   }
 
@@ -20,6 +22,22 @@ public class MapTaskFactory extends AbstractTaskFactory {
 
   @Override
   public boolean isReady() {
+    return true;
+  }
+  
+  public boolean isApplicable(CyTable table) {
+    var networkTableManager = registrar.getService(CyNetworkTableManager.class);
+    var type = networkTableManager.getTableType(table);
+    return CyEdge.class.equals(type);
+  }
+
+  @Override
+  public TaskIterator createTaskIterator(CyTable table) {
+    return new TaskIterator(new MapTask(registrar));
+  }
+
+  @Override
+  public boolean isReady(CyTable table) {
     return true;
   }
 }
